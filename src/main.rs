6ad1111303014f5001eb15d9c4fa8c36cbb3b422
Jviguy <jviguytwo2@gmail.com>
2021-02-23 00:21:46 -0500

    beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use inputbot::{KeybdKey::*, MouseButton::*};
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    let events_loop = glium::glutin::event_loop::EventLoop::new();
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world");
+    let cb = glium::glutin::ContextBuilder::new().with_vsync(true);
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+    let mut frame = display.draw();
+    frame.finish().unwrap();
+    WKey.bind(|| {
+        println!("Forward Key has been pressed!");
+    });
+    RightButton.bind(|| {
+        println!("Right mouse button has been clicked!");
+    });
+    inputbot::handle_input_events();
 }
