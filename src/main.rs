693c84804c791e4fba933d95c82ee6b2f5760957
Jviguy <jviguytwo2@gmail.com>
2021-02-23 00:58:47 -0500

    update opengl stuff for tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use inputbot::{KeybdKey::*, MouseButton::*};
 extern crate glium;
+use glium::Surface;
 
 fn main() {
-    let mut events_loop = glium::glutin::event_loop::EventLoop::new();
+    let events_loop = glium::glutin::event_loop::EventLoop::new();
     let wb = glium::glutin::window::WindowBuilder::new()
         .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
         .with_title("Hello world");
@@ -14,6 +15,10 @@ fn main() {
     LeftButton.bind(|| println!("LMB has been pressed"));
     inputbot::handle_input_events();
     events_loop.run(move |ev, _, control_flow| {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
         let next_frame_time =
             std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glium::glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
